d6fa6e6cace81d81b4e5ed33ca85886cbef981be
marcpaul <marcpaul@users.noreply.github.com>
2017-12-31 11:35:38 +0100

    removed js-data

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import TextField from 'material-ui/TextField'
 
+export class SavedUserKeyField extends React.Component {
+  render () {
+    return (
+      
+    )
+  }
+}
+
 export class SearchBar extends React.Component {
   render () {
     return (
